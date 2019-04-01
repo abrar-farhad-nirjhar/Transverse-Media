4ae4fd94d5216278075da9cbec811f86d5c0c115
abrar-farhad-nirjhar <abrar.farhad.af@gmail.com>
2019-04-01 10:23:26 +0600

    Test

---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,16 @@
+
+
+from django.urls import path, include
+from rest_framework import routers
+
+
+from .api import LeadViewset
+
+
+router = routers.DefaultRouter()
+
+router.register('api/leads', LeadViewset, 'leads')
+urlpatterns = [
+    
+
+]+router.urls
